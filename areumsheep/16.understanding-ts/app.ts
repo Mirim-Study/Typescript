637ac34dd8d1a850679ef1a70e55617b0412a951
areumsheep <areumsheep@gmail.com>
2022-05-25 11:44:16 +0900

    🎨 Add: 20강 코드 추가

--- areumsheep/16.understanding-ts/app.ts
+++ areumsheep/16.understanding-ts/app.ts
@@ -1,17 +1,26 @@
 // const person: {
 //   name: string;
 //   age: number;
+//   hobbies: string[];
+//   role: [number, string];
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY = 8, AUTHOR };
+
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN,
 };
 
 let favoriteActivities: string[];
@@ -21,4 +30,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is admin');
 }
