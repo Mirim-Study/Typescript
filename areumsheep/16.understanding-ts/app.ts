c62559ab84b1bae275564d1aafa29be6d66fa61b
areumsheep <areumsheep@gmail.com>
2022-05-25 11:33:28 +0900

    🎨 Add: 19강 코드 추가

--- areumsheep/16.understanding-ts/app.ts
+++ areumsheep/16.understanding-ts/app.ts
@@ -2,10 +2,16 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Maximilian',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
 
 let favoriteActivities: string[];
